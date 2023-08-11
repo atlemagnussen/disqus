e3b4c6c9f840725b50ddc3a95135927f9bb56e29
Atle Magnussen <atlemagnussen@gmail.com>
2023-08-11 10:32:15 +0200

    dsf

--- server/crawler.ts
+++ server/crawler.ts
@@ -1,21 +1,46 @@
 import { logger } from "./logger"
 import { getPostsFromForum } from "./disqus"
+import { SaveComment }from "./mongo"
 import { DisqusPostsResponse } from "@common/types"
 
 logger.info("crawler starting")
 
+const FORUM = "itavisen"
 
 async function crawl() {
     const res = await getPosts()
 
-
+    const result = await processFetchedPosts(res)
+    logger.info("result of saving posts", result)
     
+    logger.info("cursor", res.cursor)
 }
 
 async function getPosts() {
-    const res = await getPostsFromForum("itavisen")
+    const res = await getPostsFromForum(FORUM)
     const resJson = JSON.parse(res) as DisqusPostsResponse
     return resJson
+}
+
+async function processFetchedPosts(res: DisqusPostsResponse) {
+    let result = {
+        total: 0,
+        saved: 0
+    }
+    if (!res || !res.response || res.response.length === 0) {
+        logger.warn("No posts in res")
+        return result
+    }
+    logger.info(`Starting saving posts, count=${res.response.length}`)
+    result.total = res.response.length
+
+    for (let i = 0; i < res.response.length; i++) {
+        let post = res.response[i]
+        let saved = await SaveComment(FORUM, post)
+        if (saved)
+            result.saved += 1
+    }
+    return result
 }
 
 crawl().catch(er => {
