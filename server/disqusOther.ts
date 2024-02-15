90ffd09cf4c67ac158a1dc6b1251e128f1e0885a
Atle Magnussen <atlemagnussen@gmail.com>
2024-02-15 23:31:33 +0100

    d

---
+++ server/disqusOther.ts
@@ -0,0 +1,28 @@
+import { getHttps } from "./httpPromise"
+import config from "./config"
+
+const BASEURL = "https://disqus.com/api/3.0"
+
+
+// const FORUM = "itavisen"
+// const THREADID = "6607646921";
+
+/** No access */
+export async function listForumsByUserId(user: number) {
+    const url = `${BASEURL}/users/listForums.json?api_key=${config.apiKey}&user=${user}`
+    const res = await getHttps(url)
+    return res
+}
+
+export async function detailsOnUser(user: number) {
+    const url = `${BASEURL}/users/details.json?api_key=${config.apiKey}&user=${user}`
+    const res = await getHttps(url)
+    return res
+}
+
+/** You do not have enough privileges to access this resource. */
+export async function listMostActiveForumsOnUser(user: number) {
+    const url = `${BASEURL}/users/listActivity.json?api_key=${config.apiKey}&user=${user}`
+    const res = await getHttps(url)
+    return res
+}
